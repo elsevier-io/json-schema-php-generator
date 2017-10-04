e57882e8e2bd33035bc5384e84870f5c1376fa05
Andy Clyde <a.clyde@elsevier.com>
2017-10-04 11:22:00 +0100

    Refactor scalar properties

--- src/Properties/BooleanProperty.php
+++ src/Properties/BooleanProperty.php
@@ -2,62 +2,13 @@
 
 namespace Elsevier\JSONSchemaPHPGenerator\Properties;
 
-use Nette\PhpGenerator\ClassType;
-use Nette\PhpGenerator\Method;
-
-class BooleanProperty implements Property
+class BooleanProperty extends ScalarProperty
 {
-    /**
-     * @var string
-     */
-    private $name;
-
     /**
      * @param string $name
      */
     public function __construct($name)
     {
-        $this->name = $name;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function constructorBody() {
-        return '$this->' . $this->name . ' = $' . $this->name . ';' . "\n";
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function constructorComment()
-    {
-        return '@param boolean $' . $this->name;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function addConstructorParameter(Method $constructor) {
-        $constructor->addParameter($this->name);
-        return $constructor;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function addTo(ClassType $class)
-    {
-        $class->addProperty($this->name)
-            ->setVisibility('private')
-            ->addComment("@var boolean");
-        return $class;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function serializingCode(){
-        return "    '" . $this->name . "'=>" . '$this->' . $this->name . ",\n";
+        parent::__construct($name, 'boolean');
     }
 }
---
+++ src/Properties/ScalarProperty.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Elsevier\JSONSchemaPHPGenerator\Properties;
+
+use Nette\PhpGenerator\ClassType;
+use Nette\PhpGenerator\Method;
+
+class ScalarProperty implements Property
+{
+    /**
+     * @var string
+     */
+    private $name;
+    /**
+     * @var string
+     */
+    private $type;
+
+    /**
+     * @param string $name
+     * @param string $type
+     */
+    public function __construct($name, $type)
+    {
+        $this->name = $name;
+        $this->type = $type;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function constructorBody() {
+        return '$this->' . $this->name . ' = $' . $this->name . ';' . "\n";
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function constructorComment()
+    {
+        return '@param ' . $this->type . ' $' . $this->name;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function addConstructorParameter(Method $constructor) {
+        $constructor->addParameter($this->name);
+        return $constructor;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function addTo(ClassType $class)
+    {
+        $class->addProperty($this->name)
+            ->setVisibility('private')
+            ->addComment("@var " . $this->type);
+        return $class;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function serializingCode(){
+        return "    '" . $this->name . "'=>" . '$this->' . $this->name . ",\n";
+    }
+}
